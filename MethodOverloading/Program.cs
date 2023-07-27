346ccd69d972b4930edac753569ae7a67546acef
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-07-27 12:50:58 -0400

    Created exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine(Add(3, 4));
+            Console.WriteLine(Add(3.00m, 3.00m));
+            Console.WriteLine(Add(1, 4, true));
+        }
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool equals)
+        {
+            var sum = a + b;
+            if (equals == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (equals == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return $"{sum}";
+            }
         }
     }
 }
